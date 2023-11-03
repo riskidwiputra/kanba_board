2ac5f3c81281320cb9f8d318b22beaf888e435a7
riskidwiputra <riskidwiputra1250@gmail.com>
2023-11-03 17:23:25 +0700

    change seeder

--- database/seeders/BoardSeeder.php
+++ database/seeders/BoardSeeder.php
@@ -21,45 +21,45 @@ class BoardSeeder extends Seeder
     public function run()
     {
 
-        $board = Board::factory()->create([
-            'id_user' => User::first()->id
-        ]);
+        // $board = Board::factory()->create([
+        //     'id_user' => User::first()->id
+        // ]);
 
-        $collaborators = User::factory()->create();
+        // $collaborators = User::factory()->create();
 
-        $board->collaborators()->attach($collaborators->id);
+        // $board->collaborators()->attach($collaborators->id);
 
-        $list1 = Lists::factory()->create([
-            'name' => 'Backlog',
-            'id_board' => $board->id,
-        ]);
+        // $list1 = Lists::factory()->create([
+        //     'name' => 'Backlog',
+        //     'id_board' => $board->id,
+        // ]);
 
-        $list2 = Lists::factory()->create([
-            'name' => 'In Progress',
-            'id_board' => $board->id,
-        ]);
+        // $list2 = Lists::factory()->create([
+        //     'name' => 'In Progress',
+        //     'id_board' => $board->id,
+        // ]);
     
-        $list3 = Lists::factory()->create([
-            'name' => 'Success',
-            'id_board' => $board->id,
-        ]);
-        $labele = labels::factory(3)->create();
-        $lists = Lists::all();
+        // $list3 = Lists::factory()->create([
+        //     'name' => 'Success',
+        //     'id_board' => $board->id,
+        // ]);
+        // $labele = labels::factory(3)->create();
+        // $lists = Lists::all();
 
-        foreach ($lists as $list) {
-            // Buat 5 kartu untuk setiap list
-            $cardse = Cards::factory(5)
-                    ->create([
-                        'id_list' => $list->id,
-                         'id_label' => labels::inRandomOrder()->first()
-                    ]);
+        // foreach ($lists as $list) {
+        //     // Buat 5 kartu untuk setiap list
+        //     $cardse = Cards::factory(5)
+        //             ->create([
+        //                 'id_list' => $list->id,
+        //                  'id_label' => labels::inRandomOrder()->first()
+        //             ]);
 
-                    $cardse->each(function ($card) {
-                        // Dapatkan pengguna yang ingin Anda hubungkan (misalnya, secara acak)
-                        $user = User::inRandomOrder()->first();
+        //             $cardse->each(function ($card) {
+        //                 // Dapatkan pengguna yang ingin Anda hubungkan (misalnya, secara acak)
+        //                 $user = User::inRandomOrder()->first();
                 
-                        $card->assigns()->attach($user->id);
-                    });
-        }
+        //                 $card->assigns()->attach($user->id);
+        //             });
+        // }
     }
 }
